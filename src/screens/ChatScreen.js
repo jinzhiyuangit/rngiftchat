55194f360591a959208b3e9276e8ea341a9aba17
jinzhiyuangit <jin.zhiyuan@hotmail.com>
2019-01-01 21:43:31 +0800

    init ok

--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -1,37 +1,75 @@
 import React from "react";
+import { GiftedChat } from "react-native-gifted-chat";
+import Chatkit from "@pusher/chatkit-client";
 
-import BaseNav from './BaseNav';
-import Ionicons from 'react-native-vector-icons/Ionicons';
+const CHATKIT_INSTANCE_LOCATOR = "d6b422d1-6fd4-4271-8953-4bd711965dbd";
+const CHATKIT_TOKEN_PROVIDER_ENDPOINT =
+`https://us1.pusherplatform.io/services/chatkit_token_provider/v1/${CHATKIT_INSTANCE_LOCATOR}/token`;
+const CHATKIT_ROOM_ID = "19375885";
+const CHATKIT_USER_NAME = "zhiyuan";
 
+export default class MyChat extends React.Component {
+  state = {
+    messages: []
+  };
 
-export default class ChatScreen extends React.Component {
-    static navigationOptions = {
-      tabBarLabel: 'Chat',
-      tabBarIcon: ({ tintColor, focused, horizontal }) => (
-        <Ionicons
-          name={'ios-chatboxes'}
-          size={horizontal ? 20 : 26}
-          style={{ color: tintColor }}
-        />
-      ),
-    };
-    componentDidMount() {
-      // this._s0 = this.props.navigation.addListener('willFocus', this._onEvent);
-      // this._s1 = this.props.navigation.addListener('didFocus', this._onEvent);
-      // this._s2 = this.props.navigation.addListener('willBlur', this._onEvent);
-      // this._s3 = this.props.navigation.addListener('didBlur', this._onEvent);
-    }
-    componentWillUnmount() {
-      // this._s0.remove();
-      // this._s1.remove();
-      // this._s2.remove();
-      // this._s3.remove();
-    }
-    _onEvent = a => {
-      console.log('EVENT ON CHAT TAB', a.type, a);
-    };
-    render() {
-      const { navigation } = this.props;
-      return <BaseNav banner="Chat Tab" navigation={navigation} />;
-    }
+  componentDidMount() {
+    const tokenProvider = new Chatkit.TokenProvider({
+      url: CHATKIT_TOKEN_PROVIDER_ENDPOINT
+    });
+
+    const chatManager = new Chatkit.ChatManager({
+      instanceLocator: `v1:us1:${CHATKIT_INSTANCE_LOCATOR}`,
+      userId: CHATKIT_USER_NAME,
+      tokenProvider: tokenProvider
+    });
+
+    chatManager.connect().then(currentUser => {
+      this.currentUser = currentUser;
+      this.currentUser.subscribeToRoom({
+        roomId: CHATKIT_ROOM_ID,
+        hooks: {
+          onMessage: this.onReceive.bind(this)
+        }
+      });
+    });
   }
+
+  onReceive(data) {
+    const { id, senderId, text, createdAt } = data;
+    const incomingMessage = {
+      _id: id,
+      text: text,
+      createdAt: new Date(createdAt),
+      user: {
+        _id: senderId,
+        name: senderId,
+        avatar:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmXGGuS_PrRhQt73sGzdZvnkQrPXvtA-9cjcPxJLhLo8rW-sVA"
+      }
+    };
+
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, incomingMessage)
+    }));
+  }
+
+  onSend([message]) {
+    this.currentUser.sendMessage({
+      text: message.text,
+      roomId: CHATKIT_ROOM_ID
+    });
+  }
+
+  render() {
+    return (
+      <GiftedChat
+        messages={this.state.messages}
+        onSend={messages => this.onSend(messages)}
+        user={{
+          _id: CHATKIT_USER_NAME
+        }}
+      />
+    );
+  }
+}
